9a09fcfe7d68ef7d8ca6309487336646d290ed74
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-03-18 00:11:43 +0100

    completed!

--- 1/prova.m
+++ 1/prova.m
@@ -1,7 +1,25 @@
 % Load the dataframe from the CSV file
 data = readtable('C:\Users\giord\OneDrive\Desktop\UNI\MAGISTRALE\Intelligent Systems For Pattern Recognition\Assignments\PatternRecognition\1\data\1.csv');
 
-col = 2;
+col = 2; %from 2 to 4: x, y, z 
+
+% Define a structure where each field represents an activity ID and its value is the corresponding description
+activity_descriptions = {
+    'Working at Computer';                             % Activity 1
+    'Standing Up, Walking and Going up/down stairs';   % Activity 2
+    'Standing';                                        % Activity 3
+    'Walking';                                         % Activity 4
+    'Going Up/Down Stairs';                            % Activity 5
+    'Walking and Talking with Someone';                % Activity 6
+    'Talking while Standing'                           % Activity 7
+};
+
+axis_descriptions = {
+    'duh';
+    'X';
+    'Y';
+    'Z';
+};
 
 % Assuming each column is a variable you want to analyze with CWT
 
@@ -15,7 +33,7 @@ columnData = table2array(data(:, col));
 figure; % Open a new figure window
 imagesc(abs(cfs));  % Visualize the absolute value of the CWT coefficients
 colorbar;  % Show a color bar indicating the coefficient magnitudes
-title(['CWT of Column ', num2str(col)]);
+title(['Complete CWT of ', axis_descriptions{col}, ' axis']);
 xlabel('Time');
 ylabel('Scale');
 axis tight; % Fit the axes tightly around the data
@@ -34,13 +52,13 @@ for activity_label_index = 1:numel(activity_labels)
     columnData = table2array(activity_data(:, col));
     
     % Perform the Continuous Wavelet Transform on the column data
-    [cfs, frequencies] = cwt(columnData, 'bump'); % Example uses the Morlet (analytic Morlet) wavelet
+    [cfs, frequencies] = cwt(columnData, 'amor'); % Example uses the Morlet (analytic Morlet) wavelet
 
     % Plot the CWT result for this column
     figure; % Open a new figure window
     imagesc(abs(cfs));  % Visualize the absolute value of the CWT coefficients
     colorbar;  % Show a color bar indicating the coefficient magnitudes
-    title(['CWT of Column ', num2str(col), ' for Activity ', (activity_labels(activity_label_index))]);
+    title(['CWT of ', axis_descriptions{col}, ' axis for Activity ', activity_descriptions{activity_label_index}]);
     xlabel('Time');
     ylabel('Scale');
     axis tight; % Fit the axes tightly around the data
