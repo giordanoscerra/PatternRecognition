94b82314bbfa0397ab9d776e849d33b48599e784
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-03-17 23:54:52 +0100

    matlab code for activity cwt

---
+++ 1/prova.m
@@ -0,0 +1,31 @@
+% Load the dataframe from the CSV file
+data = readtable('C:\Users\giord\OneDrive\Desktop\UNI\MAGISTRALE\Intelligent Systems For Pattern Recognition\Assignments\PatternRecognition\1\data\1.csv');
+
+col = 2;
+
+
+% Extract unique activity labels from the 5th column
+activity_labels = unique(data{:, 5});
+activity_labels = activity_labels(2:end);
+
+% Loop through each unique activity label
+for activity_label_index = 1:numel(activity_labels)
+    % Extract data corresponding to the current activity label
+    activity_data = data(data{:, 5} == activity_labels(activity_label_index), :);
+
+    % Extract the column data for the current variable
+    columnData = table2array(activity_data(:, col));
+    
+    % Perform the Continuous Wavelet Transform on the column data
+    [cfs, frequencies] = cwt(columnData, 'bump'); % Example uses the Morlet (analytic Morlet) wavelet
+
+    % Plot the CWT result for this column
+    figure; % Open a new figure window
+    imagesc(abs(cfs));  % Visualize the absolute value of the CWT coefficients
+    colorbar;  % Show a color bar indicating the coefficient magnitudes
+    title(['CWT of Column ', num2str(col), ' for Activity ', (activity_labels(activity_label_index))]);
+    xlabel('Time');
+    ylabel('Scale');
+    axis tight; % Fit the axes tightly around the data
+
+end
