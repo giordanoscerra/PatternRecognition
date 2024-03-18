8d290e0583d0a2a219779d202e0cfa4b2468614f
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-03-18 16:09:36 +0100

    comments

--- 1/main.m
+++ 1/main.m
@@ -2,7 +2,6 @@
 dataFolder = fullfile('data');
 plotsBaseFolder = fullfile('plots');
 
-
 % Define activity and axis descriptions
 activity_descriptions = {
     'Working at Computer';
@@ -14,6 +13,7 @@ activity_descriptions = {
     'Talking while Standing'
 };
 
+% Define axis descriptions
 axis_descriptions = {
     'duh';
     'X';
@@ -38,7 +38,7 @@ for fileNum = 1:15
         columnData = table2array(data(:, col));
         
         % Perform CWT on the column data
-        [cfs, frequencies] = cwt(columnData, 'bump');  % Or use 'amor' for Morlet
+        [cfs, frequencies] = cwt(columnData, 'bump');  % Here we use bump for more localized results
         
         % Plot and save the complete CWT result
         figure;
@@ -47,21 +47,23 @@ for fileNum = 1:15
         title(['Complete CWT of ', axis_descriptions{col}, ' axis']);
         xlabel('Time');
         ylabel('Scale');
-        axis tight;
-        
+        axis tight;        
         saveas(gcf, fullfile(currentPlotFolder, ['Complete_CWT_', axis_descriptions{col}, '_axis.png']));
         
-        % Extract unique activity labels from the 5th column, skipping the first if it's a header or NaN
+        % Extract unique activity labels from the 5th column, skipping the first (0 value is for the end of the file)
         activity_labels = unique(data{:, 5});
         activity_labels = activity_labels(2:end);
         
         % Loop through each activity label
         for activity_label_index = 1:numel(activity_labels)
+            % Extract the column data for the current activity label
             activity_data = data(data{:, 5} == activity_labels(activity_label_index), :);
             columnData = table2array(activity_data(:, col));
             
-            [cfs, frequencies] = cwt(columnData, 'amor');  % Or 'bump', depending on preference
+            % Here we can use Morlet for more frequency localized results
+            [cfs, frequencies] = cwt(columnData, 'amor'); 
             
+            % Plot and save the complete CWT result
             figure;
             imagesc(abs(cfs));
             colorbar;
@@ -69,8 +71,8 @@ for fileNum = 1:15
             xlabel('Time');
             ylabel('Scale');
             axis tight;
-            
             saveas(gcf, fullfile(currentPlotFolder, ['CWT_', axis_descriptions{col}, '_axis_Activity_', num2str(activity_label_index), '.png']));
+
         end
     end
 end
