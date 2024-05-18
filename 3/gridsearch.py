43f3dd352688128b249706783a5b72981d1ca5d7
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-05-18 13:27:48 +0200

    final touches

--- 3/gridsearch.py
+++ 3/gridsearch.py
@@ -1,39 +1,34 @@
 import subprocess
 from itertools import product
-import os
 import multiprocessing
 
-#os.chdir("./3")
-
-n_epochs_values = [500]
-hidden_size_values = [150]
-n_layers_values = [2,3]
-model_values = ["lstm"]
+dataset = "review.txt"
+n_epochs_values = [50, 100, 500, 1000, 20000]
+hidden_size_values = [150, 50, 100]
+n_layers_values = [5,4,3,2]
+model_values = ["lstm", "gru"]
 learning_rate_values = [0.01]
-chunk_len_values = [400]
+chunk_len_values = [100, 200]
 batch_size_values = [100]
-regularize_values = [0, 0.5]
-
-'''
-n_epochs_values = [1000, 2000]
-hidden_size_values = [100, 200]
-n_layers_values = [2]
-model_values = ["lstm"]
-learning_rate_values = [0.01]
-chunk_len_values = [200]
-batch_size_values = [100]
-regularize_values = [0, 0.3]
-'''
+regularize_values = [0, 0.2, 0.5]
 
 
 def run_training(n_epochs, hidden_size, n_layers, model, learning_rate, chunk_len, batch_size, regularize):
-    args = ["--n_epochs", str(n_epochs), "--hidden_size", str(hidden_size), "--n_layers", str(n_layers), "--model", model, "--learning_rate", str(learning_rate), "--chunk_len", str(chunk_len), "--batch_size", str(batch_size), "--regularize", str(regularize), "review.txt"]
+    args = ["--n_epochs", str(n_epochs), 
+            "--hidden_size", str(hidden_size), 
+            "--n_layers", str(n_layers), 
+            "--model", model, 
+            "--learning_rate", str(learning_rate), 
+            "--chunk_len", str(chunk_len), 
+            "--batch_size", str(batch_size), 
+            "--regularize", str(regularize), 
+            dataset]
     command = ["python", "train.py"] + args
     subprocess.run(command)
 
 if __name__ == "__main__":
     processes = []
-    max_processes = 1
+    max_processes = 4
     for n_epochs, hidden_size, n_layers, model, learning_rate, chunk_len, batch_size, regularize in product(n_epochs_values, hidden_size_values, n_layers_values, model_values, learning_rate_values, chunk_len_values, batch_size_values, regularize_values):
         p = multiprocessing.Process(target=run_training, args=(n_epochs, hidden_size, n_layers, model, learning_rate, chunk_len, batch_size, regularize))
         p.start()
--- 3/test_all.py
+++ 3/test_all.py
@@ -1,15 +1,20 @@
 import os
 import subprocess
 
-folder = 'review_models'
-output_file = 'output.txt'
+dataset = 'totti'
+folder = dataset+'_models'
+output_file = dataset+'_output.txt'
 i = 0
+
+temperature = "1.15"
+start_token = "Totti"
+
 with open(output_file, 'w') as f:
     for file in os.listdir(folder):
         if file.endswith('.pt'):
             i += 1
             path = os.path.join(folder, file)
-            command = ["python", "generate.py", path, "-t", "1.1", "-p", "They"]
+            command = ["python", "generate.py", path, "-t", temperature, "-p", start_token]
             f.write(str(i) + ": " + "model " + file + "\n")
             result = subprocess.run(command, capture_output=True, text=True)
             f.write("\t"+ result.stdout + "\n")
